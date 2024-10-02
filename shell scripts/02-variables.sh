18c099e61ded5a1a77ac0c55d746b3dc369844df
cloudtechprem <cloudtechprem@gmail.com>
2024-10-02 09:03:59 +0530

    2nd commit

--- shell scripts/02-variables.sh
+++ shell scripts/02-variables.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo "Ramesh: Hi Suresh, Good Morning!"
-echo "Suresh: Hi Ramesh, Very Good Morning!"
-echo "Ramesh: How are you Suresh?"
-echo "Suresh: I'm good, what about you Ramesh?"
+#PERSON1=Ramesh
+#PERSON2=Suresh 
+
+PERSON1=$1 #Here we are giving the person names in command line arguments when executing the script
+PERSON2=$2 #Here we are giving the person names in command line arguments when executing the script
+
+echo "$PERSON1: Hi $PERSON2, Good Morning!"
+echo "$PERSON2: Hi $PERSON1, Very Good Morning!"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I'm good, what about you $PERSON1?"
 
