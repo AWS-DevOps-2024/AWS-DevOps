75034e880583f4956fd3e522c0e11c8a049d14ce
cloudtechprem <cloudtechprem@gmail.com>
2024-10-02 18:36:47 +0530

    5th commit

--- shell scripts/11-logs.sh
+++ shell scripts/11-logs.sh
@@ -4,10 +4,10 @@ ID=$(id -u)
 echo "Script name :: $0" # $0 --> it denotes the script name by default
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOG_FILE="/tmp/$0-$TIMESTAMP.log"
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+R="\e[31m" #Red Colour
+G="\e[32m" #Green Colour
+Y="\e[33m" #Yellow Colour
+N="\e[0m" #No Colour
 
 VALIDATE(){
     if [ $1 -ne "0" ] # $? --> is the result of the previous command, If it is success then it will be 0 or failed then it is othan than 0 (ex: any nimber 1,34, 56 etc)
@@ -24,7 +24,7 @@ if [ $ID = "0" ] #Here "="means is equal to, we can also use "-ne" means not equ
 then
     echo "You are a root user, PROCEED"
 else
-    echo "ERROR:: You do not have root permissions to execute this"
+    echo -e "$R ERROR:: You do not have root permissions to execute this $N"
     exit 1
 fi
 
