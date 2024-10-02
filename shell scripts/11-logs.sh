864684af25a2eb81559e8958e99dec0c61a6cb72
cloudtechprem <cloudtechprem@gmail.com>
2024-10-02 18:19:20 +0530

    3rd commit

--- shell scripts/11-logs.sh
+++ shell scripts/11-logs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ID=$(id -u)
-echo "Script name=$0"
-
-#LOG_FILE=/tmp/
+echo "Script name :: $0" # $0 --> it denotes the script name by default
+TIMESTAMP=$(date +%F-%H:%M:%S)
+LOG_FILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne "0" ] # $? --> is the result of the previous command, If it is success then it will be 0 or failed then it is othan than 0 (ex: any nimber 1,34, 56 etc)
@@ -24,11 +24,11 @@ else
     exit 1
 fi
 
-yum install mysql -y
+yum install mysql -y &>> $LOG_FILE
 VALIDATE $? "Installing MySQL" #($2) # We are giving the arguments to VALIDATE function
 # In the Validate function 
     # --- $1 is $?
     # --- $2 is "Installing MySQL or GIT"
 
-yum install git -y
+yum install git -y &>> $LOG_FILE
 VALIDATE $? "Installing GIT"
