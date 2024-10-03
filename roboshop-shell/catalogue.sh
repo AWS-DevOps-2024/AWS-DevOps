409e5c46156d7df8c3e8072c2d798cfa2dc48851
cloudtechprem <cloudtechprem@gmail.com>
2024-10-03 21:51:10 +0530

    3rd commit

--- roboshop-shell/catalogue.sh
+++ roboshop-shell/catalogue.sh
@@ -28,47 +28,47 @@ then
 else
     echo -e "You are a Root User...$G PROCEED$N"
 fi
-dnf module disable nodejs -y &>> $LOG_FILE
+dnf module disable nodejs -y &>> &>> $LOG_FILE
 VALIDATE $? "Disabling NodeJs 10"
 
-dnf module enable nodejs:18 -y &>> $LOG_FILE
+dnf module enable nodejs:18 -y &>> &>> $LOG_FILE
 VALIDATE $? "Enabling NodeJs 18"
 
-dnf install nodejs -y &>> $LOG_FILE
+dnf install nodejs -y &>> &>> $LOG_FILE
 VALIDATE $? "Installing NodeJs"
 
-useradd roboshop &>> $LOG_FILE
+useradd roboshop &>> &>> $LOG_FILE
 VALIDATE $? "Adding Roboshop User"
 
-mkdir -p /app &>> $LOG_FILE
+mkdir -p /app &>> &>> $LOG_FILE
 VALIDATE $? "Create App Directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOG_FILE
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> &>> $LOG_FILE
 VALIDATE $? "Download Catalogue Application"
 
-cd /app &>> $LOG_FILE
+cd /app &>> &>> $LOG_FILE
 VALIDATE $? "Go to App Directory"
 
-unzip /tmp/catalogue.zip $LOG_FILE
+unzip /tmp/catalogue.zip &>> $LOG_FILE
 VALIDATE $? "Unzipping Catalogue Application"
 
-npm install $LOG_FILE
+npm install &>> $LOG_FILE
 VALIDATE $? "Installing NodeJS Dependencies"
 
-cp /home/centos/AWS-DevOps/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service $LOG_FILE
+cp /home/centos/AWS-DevOps/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOG_FILE
 VALIDATE $? "Copying Catalogue.service"
 
-systemctl daemon-reload  $LOG_FILE
+systemctl daemon-reload  &>> $LOG_FILE
 VALIDATE $? "Daemon Reaload"
 
-systemctl enable catalogue $LOG_FILE
+systemctl enable catalogue &>> $LOG_FILE
 VALIDATE $? "Ebnable Catalogue"
 
-systemctl start catalogue $LOG_FILE
+systemctl start catalogue &>> $LOG_FILE
 VALIDATE $? "Start Catalogue"
 
-dnf install mongodb-org-shell -y $LOG_FILE
+dnf install mongodb-org-shell -y &>> $LOG_FILE
 VALIDATE $? "Install MongoDB Client"
 
-mongo --host mongodb.learndevops.space </app/schema/catalogue.js $LOG_FILE
+mongo --host mongodb.learndevops.space </app/schema/catalogue.js &>> $LOG_FILE
 VALIDATE $? "Pushing Products into MongoDB Server" 
