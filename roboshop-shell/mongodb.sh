b6abbd49230b2e2b53a4def437ba2b5f852f4623
cloudtechprem <cloudtechprem@gmail.com>
2024-10-03 17:06:04 +0530

    3rd commit

--- roboshop-shell/mongodb.sh
+++ roboshop-shell/mongodb.sh
@@ -12,6 +12,7 @@ VALIDATE() {
     if [ $1 -ne "0" ]
     then    
         echo -e "$2 is...$R FAILED$N"
+        exit 1
     else
         echo -e "$2 is...$G SUCCESS$N"
     fi
@@ -22,6 +23,7 @@ VALIDATE() {
 if [ $(whoami) -ne "root" ]
 then    
     echo -e "$R ERROR$N:: You are not a Root User, Please run this with root access."
+    exit 1
 else
     echo -e "You are a Root User...$G PROCEED$N"
 fi
