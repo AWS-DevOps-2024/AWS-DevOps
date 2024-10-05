9a4ba518b6ebde50b033f2a1165ec473e763f9a2
cloudtechprem <cloudtechprem@gmail.com>
2024-10-05 15:49:34 +0530

    1st commit

--- shell scripts/14-delete-old-logs.sh
+++ shell scripts/14-delete-old-logs.sh
@@ -12,11 +12,11 @@ then
     echo -e "Source Directory $R $SOURCE_DIR $N does not Exist"
 fi
 
-#Find files to delete
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+# #Find files to delete
+# FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
-while IFS= read -r line
-do 
-    echo "Deleting file:: $line"
-    rm -rf $line
-done <<< $FILES_TO_DELETE
+# while IFS= read -r line
+# do 
+#     echo "Deleting file:: $line"
+#     rm -rf $line
+# done <<< $FILES_TO_DELETE
