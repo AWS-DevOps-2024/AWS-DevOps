82cc9316decc313ddbf496e1fc494e5606285a86
cloudtechprem <cloudtechprem@gmail.com>
2024-10-05 10:39:09 +0530

    2nd commit

--- roboshop-shell/payment.sh
+++ roboshop-shell/payment.sh
@@ -29,10 +29,10 @@ else
     echo -e "You are a Root User...$G PROCEED$N"
 fi
 
-dnf install python36 gcc python3-devel -y
+dnf install python36 gcc python3-devel -y &>> $LOG_FILE
 VALIDATE $? "Installing Python Package"
 
-id roboshop
+id roboshop &>> $LOG_FILE
 if [ $? -ne "0" ]
 then 
     useradd roboshop &>> $LOG_FILE
@@ -41,28 +41,28 @@ else
     echo -e "Roboshop User already Exist...$Y SKIPPING$N"
 fi 
 
-mkdir -p /app 
+mkdir -p /app &>> $LOG_FILE
 VALIDATE $? "Creating App directory"
 
-curl -L -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip
+curl -L -o /tmp/payment.zip https://roboshop-builds.s3.amazonaws.com/payment.zip &>> $LOG_FILE
 VALIDATE $? "Downloading payment.zip"
 
 cd /app 
 
-unzip /tmp/payment.zip
+unzip /tmp/payment.zip &>> $LOG_FILE
 VALIDATE $? "Unzipping payment.zip"
 
-pip3.6 install -r requirements.txt
+pip3.6 install -r requirements.txt &>> $LOG_FILE
 VALIDATE $? "Installing Dependencies"
 
-cp /home/centos/AWS-DevOps/roboshop-shell/payment.service /etc/systemd/system/payment.service
+cp /home/centos/AWS-DevOps/roboshop-shell/payment.service /etc/systemd/system/payment.service &>> $LOG_FILE
 VALIDATE $? "Copying payment.service file"
 
-systemctl daemon-reload
+systemctl daemon-reload &>> $LOG_FILE
 VALIDATE $? "daemon Reaload"
 
-systemctl enable payment 
+systemctl enable payment &>> $LOG_FILE
 VALIDATE $? "Enabling Payment"
 
-systemctl start payment 
+systemctl start payment &>> $LOG_FILE
 VALIDATE $? "Starting Payment"
