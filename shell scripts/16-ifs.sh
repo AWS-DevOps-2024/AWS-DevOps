263ab79ef07295ea0165c257a3af311b531b913d
cloudtechprem <cloudtechprem@gmail.com>
2024-10-07 09:10:31 +0530

    1st commit

---
+++ shell scripts/16-ifs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+file="/etc/passwd"
+
+while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
+do
+    echo "Username:: $username"
+    echo "User Full Name:: $user_fullname"
+    echo "User ID:: $user_id
+done <<< $file
