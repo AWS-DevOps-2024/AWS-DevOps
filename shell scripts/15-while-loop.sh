16b0b93950feabb968efa7c9c858b8280f6d15af
cloudtechprem <cloudtechprem@gmail.com>
2024-10-07 08:33:25 +0530

    1st commit

--- shell scripts/15-while-loop.sh
+++ shell scripts/15-while-loop.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+R="\e[31m"
+N="\e[0m"
+
 FILE_TO_READ=$(cat sample.txt) 
 
 while IFS= read -r line
 do
-    echo "$line"
+    echo "Deleting the line:: $R $line $N"
+
 done <<< $FILE_TO_READ
