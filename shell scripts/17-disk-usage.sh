05c64444caf10e10618748f3514c5f33ea465943
cloudtechprem <cloudtechprem@gmail.com>
2024-10-07 11:50:00 +0530

    3rd commit

--- shell scripts/17-disk-usage.sh
+++ shell scripts/17-disk-usage.sh
@@ -5,15 +5,16 @@ N="\e[0m"
 
 DISK_USAGE=$(df -hT | grep xvd)
 DISK_THRESHOLD=1
-MESSAGE=""
+message=""
 
 while IFS= read -r line
 do
     usage=$(echo df -hT | grep xvd | awk '{print $6F}')
     partition=$(echo df -hT | grep xvd | awk '{print $1F}')
-    echo "$line"
-    # if [ $usage -gt $DISK_THRESHOLD ]
-    # then
-    #     message+="High Disk Usage on $partition::$usage"
+
+    if [ $usage -gt $DISK_THRESHOLD ]
+    then
+        message+="High Disk Usage on $partition::$usage\n"
 done <<< $DISK_USAGE
 
+    echo "$message"
