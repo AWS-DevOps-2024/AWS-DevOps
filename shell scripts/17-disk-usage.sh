c7a4f145bec40ba9af5366ed7153a218d4625b64
cloudtechprem <cloudtechprem@gmail.com>
2024-10-07 12:24:07 +0530

    8th commit

--- shell scripts/17-disk-usage.sh
+++ shell scripts/17-disk-usage.sh
@@ -7,7 +7,7 @@ DISK_USAGE=$(df -hT | grep xvd)
 DISK_THRESHOLD=1
 # message=""
 
-while IFS= read line
+while IFS= read -r line
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
