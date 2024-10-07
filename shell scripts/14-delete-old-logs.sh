904b073ae4c3ba377feccb8e8053e6d253b0abe0
cloudtechprem <cloudtechprem@gmail.com>
2024-10-07 08:20:39 +0530

    2nd commit

--- shell scripts/14-delete-old-logs.sh
+++ shell scripts/14-delete-old-logs.sh
@@ -8,18 +8,25 @@ N="\e[0m"
 # SOURCE_DIR="/tmp/shell-logs"
 SOURCE_DIR=$1
 
+# if [ ! -d $SOURCE_DIR ]
+# then
+#     echo -e "Source Directory $R $SOURCE_DIR $N does not Exist"
+# else
+#     echo -e "Source Directory $G $SOURCE_DIR $N Exists"
+# fi
+
+# #Find files to delete
+
 if [ ! -d $1 ]
 then
-    echo -e "Source Directory $R $1 $N does not Exist"
+    echo "Source Directory $1 does not Exist"
 else
-    echo -e "Source Directory $G $1 $N Exists"
+    echo "Source Directory is Valid"
 fi
 
-#Find files to delete
-FILES_TO_DELETE=$(find $1 -type f -mtime +14 -name "*.log")
+FILES_TO_DELET=$(find $1 -type f -mtime +15 -name "*.log")
 
 while IFS= read -r line
-do 
-    echo "Deleting file:: $line"
-    rm -rf $line
+do
+    echo "Deleting File:: $line"
 done <<< $FILES_TO_DELETE
