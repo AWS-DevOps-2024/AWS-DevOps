549a5db5589d7ee5ed224323754a1b4c8f1297d0
cloudtechprem <cloudtechprem@gmail.com>
2024-10-08 09:05:51 +0530

    2nd commit

--- shell scripts/18-postfix.sh
+++ shell scripts/18-postfix.sh
@@ -38,10 +38,10 @@ VALIDATE $? "Restarting PostFix"
 systemctl enable postfix &>> $LOG_FILE
 VALIDATE $? "Enabling PostFix"
 
-cp -o /home/centos/AWS-DevOps/shell scripts/postfix-main.cf /etc/postfix/main.cf &>> $LOG_FILE
+cp /home/centos/AWS-DevOps/shell scripts/postfix-main.cf /etc/postfix/main.cf &>> $LOG_FILE
 VALIDATE $? "Copying main.cf file"
 
-cp -o /home/centos/AWS-DevOps/shell scripts/sasl_passwd /etc/postfix/sasl_passwd &>> $LOG_FILE
+cp /home/centos/AWS-DevOps/shell scripts/sasl_passwd /etc/postfix/sasl_passwd &>> $LOG_FILE
 VALIDATE $? "Copying sasl_passwd file"
 
 postmap /etc/postfix/sasl_passwd &>> $LOG_FILE
