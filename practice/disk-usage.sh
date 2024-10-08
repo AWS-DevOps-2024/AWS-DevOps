cf995d1918564d46d78ca7c2e3587547754f817b
cloudtechprem <cloudtechprem@gmail.com>
2024-10-08 09:43:44 +0530

    1st commit

---
+++ practice/disk-usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+THRESHOLD=1
+
+while IFS= read -r line
+do
+    usage=$(echo $line | awk '{print 6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print 1F}')
+    if [ $usage -ge $THRESHOLD ]
+    then    
+        message+=$("echo High disk Usage is on $partition:: $usage% \n")
+done <<< $DISK_USAGE
+
+echo -e "$usage"
