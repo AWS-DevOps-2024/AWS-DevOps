9f86fd6a2272be013f5f9f25c08dc0b5c9e1d395
cloudtechprem <cloudtechprem@gmail.com>
2024-10-08 09:54:07 +0530

    3rd commit

--- practice/disk-usage.sh
+++ practice/disk-usage.sh
@@ -5,8 +5,8 @@ THRESHOLD=1
 
 while IFS= read -r line
 do
-    usage=$(echo $line | awk '{print 6F}' | cut -d % -f1)
-    partition=$(echo $line | awk '{print 1F}')
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $THRESHOLD ]
     then    
         message+="High disk Usage is on $partition:: $usage% \n"
