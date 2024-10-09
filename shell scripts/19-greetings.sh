aa4052f5efb96a35512810ecebb98b5bacfbc129
cloudtechprem <cloudtechprem@gmail.com>
2024-10-09 09:41:49 +0530

    7th commit

--- shell scripts/19-greetings.sh
+++ shell scripts/19-greetings.sh
@@ -16,9 +16,9 @@ while getopts ":n:w:h" opt; do
     case $opt in 
         n) NAME="$OPTARG";;
         w) WISHES="$OPTARG";;
-        \?) echo "Invalid Options: -"$OPTARG"" >&2; USAGE; exit;; #Inavlid Options
-        h) USAGE; exit;; 
         :) echo "Option requires an argument: -"$OPTARG"" >&2; #empty Options
+        \?) echo "Invalid Options: -"$OPTARG"" >&2; USAGE; exit;; #Inavlid Options
+        h) USAGE; exit;;   
     esac
 done
 
