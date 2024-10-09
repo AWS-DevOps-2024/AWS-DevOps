ba904aa0b3311c526a8e2a5e7065e636d3d30f11
cloudtechprem <cloudtechprem@gmail.com>
2024-10-09 09:11:45 +0530

    3rd commit

--- shell scripts/19-greetings.sh
+++ shell scripts/19-greetings.sh
@@ -6,16 +6,19 @@ WISHES=""
 USAGE(){
     echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
     echo "Options"
-    echo "-n, Specify the name"
-    echo "-w, Specify the wishes"
-    echo "-h, Display the help and exit"
+    echo " -n, Specify the name"
+    echo " -w, Specify the wishes"
+    echo " -h, Display the help and exit"
 }
 
 while getopts ":n:w:h" opt; do 
     case $opt in 
         n) NAME="$OPTARG";;
         w) WISHES="$OPTARG";;
-        n|*) USAGE; exit;;
+        \?) echo "Invalid Options: -"$OPTARG"" >&2; USAGE; exit;; #Inavlid Options
+        h) USAGE; exit;; 
+        :) USAGE; exit;; #empty Options
     esac
 done
+
 echo "Hello $NAME, $WISHES" 
