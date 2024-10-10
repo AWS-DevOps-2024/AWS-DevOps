46e4fb0e776830e627d6058ea86d1cc6ea0bbd79
cloudtechprem <cloudtechprem@gmail.com>
2024-10-10 22:38:17 +0530

    7th commit

--- practice/delete-archive-logs.sh
+++ practice/delete-archive-logs.sh
@@ -93,7 +93,7 @@ else
     while IFS= read -r line
     do
         echo "Archiving the following Files:: $line"
-        zip -r "$dstination_dir/$(basename $line).zip"
+        zip -r "$destination_dir/archive.zip"
         rm -rf $line
     done <<< $FILES_TO_ARCHIVE
 fi
