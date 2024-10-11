08bc5c6bee36fc4fd45994d0f4e8cb0b8612debb
cloudtechprem <cloudtechprem@gmail.com>
2024-10-11 19:53:09 +0530

    1st commit

--- practice/roboshop-ec2-creation.sh
+++ practice/roboshop-ec2-creation.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# instances=("mongodb" "web" "mysql" )
+instances=("mongodb" "web" "mysql" )
+instance_type="t2.micro"
 
-# aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type t2.micro --security-group-ids sg-04d3cc3675c0c646f --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instances[0]}]'
+for i in "$instances[@]"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ]
+    then instance_type="t2.small"
+    fi
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $instance_type --security-group-ids sg-04d3cc3675c0c646f --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instances }]'
+done
 
-aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type t2.micro --security-group-ids sg-04d3cc3675c0c646f --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Web}]'
+
+# aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type t2.micro --security-group-ids sg-04d3cc3675c0c646f --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Web}]'
