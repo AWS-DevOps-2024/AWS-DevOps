a29d5ce4d40812c0da547ef2a25549dc41ee8ec7
cloudtechprem <cloudtechprem@gmail.com>
2024-10-11 19:59:30 +0530

    1st commit

--- practice/roboshop-ec2-creation.sh
+++ practice/roboshop-ec2-creation.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 instances=("mongodb" "web" "mysql" )
-instance_type="t2.micro"
+
 
 for i in "$instances[@]"
 do
     if [ $i == "mongodb" ] || [ $i == "mysql" ]
-    then instance_type="t2.small"
+    then 
+        instance_type="t2.small"
+    else
+        instance_type="t2.micro"
     fi
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $instance_type --security-group-ids sg-04d3cc3675c0c646f --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instances[@] }]'
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $instance_type --security-group-ids sg-04d3cc3675c0c646f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
 
 
