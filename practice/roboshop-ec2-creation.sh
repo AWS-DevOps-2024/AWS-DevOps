3d10e3e8d09f94d558c707602d34e294d017b420
cloudtechprem <cloudtechprem@gmail.com>
2024-10-14 09:12:47 +0530

    2nd commit

--- practice/roboshop-ec2-creation.sh
+++ practice/roboshop-ec2-creation.sh
@@ -17,7 +17,7 @@ do
     fi
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $instance_type --security-group-ids sg-04d3cc3675c0c646f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo -e "$i:: $G$IP_ADDRESS$N"
-done
+
 
 
 # aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type t2.micro --security-group-ids sg-04d3cc3675c0c646f --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Web}]'
@@ -39,3 +39,5 @@ aws route53 change-resource-record-sets \
       }
     }]
   }'
+
+done
